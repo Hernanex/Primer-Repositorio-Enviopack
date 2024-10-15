2405ba3bc196cae129704c98176acde7b5e7b8bf
Ignacio Toledo Roll <rolltoledoignacio@gmail.com>
2024-10-15 15:29:22 -0300

    Agrego un try/catch para manejo de excepciones en los test

--- src/test/java/com/enviopack/tests/LoginTest.java
+++ src/test/java/com/enviopack/tests/LoginTest.java
@@ -1,24 +1,32 @@
 package com.enviopack.tests;
 
-
 import org.testng.annotations.Test;
 import com.enviopack.common.BaseTest;
 import com.enviopack.helpers.ValidationHelper;
-
 
 public class LoginTest extends BaseTest {
 
     @Test
     public void loginAdmin() {
-    	performLoginAdmin();
-    	
-    	 ValidationHelper.validateUrlContainsWithWait(getDriver(), "backoffice", 5);
+        String expectedText = "backoffice";
+        try {
+            performLoginAdmin();
+            ValidationHelper.validateUrlContainsWithWait(getDriver(), expectedText, 5);
+        } catch (AssertionError e) {
+            String actualUrl = getDriver().getCurrentUrl();
+            throw new AssertionError("Error: Se esperaba que la URL contuviera '" + expectedText + "', pero la URL obtenida fue '" + actualUrl + "'");
+        }
     }
 
     @Test
     public void loginSeller() {
-    	performLoginSeller();
-    	
-    	ValidationHelper.validateUrlContainsWithWait(getDriver(), "ordenes", 5);
+        String expectedText = "ordenes";
+        try {
+            performLoginSeller();
+            ValidationHelper.validateUrlContainsWithWait(getDriver(), expectedText, 5);
+        } catch (AssertionError e) {
+            String actualUrl = getDriver().getCurrentUrl();
+            throw new AssertionError("Error: Se esperaba que la URL contuviera '" + expectedText + "', pero la URL obtenida fue '" + actualUrl + "'");
+        }
     }
 }
